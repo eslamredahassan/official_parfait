78df07a0f7a098a960d12ff709d2e2d0b60fa677
Es lam <freelancer.eslamreda@gmail.com>
2023-10-06 00:23:18 +0000

    Update logo.js

--- src/assest/logo.js
+++ src/assest/logo.js
@@ -16,7 +16,7 @@ console.log(
   `\x1b[33m`,
   `\x1b[7m`,
   `\x1b[1m`,
-  `                     PARFAIT™ DEV VERSION BY EGYGAMER                   `,
+  `                  PARFAIT OFFICIAL VERSION BY EGYGAMER                 `,
 );
 console.log(
   `\x1b[0m`,
