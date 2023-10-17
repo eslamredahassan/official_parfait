e504e43a1a2147838b58788ee367aeab92d596e5
Es lam <freelancer.eslamreda@gmail.com>
2023-10-17 16:04:32 +0000

    Update answer_yes.js

--- src/buttons/answer_yes.js
+++ src/buttons/answer_yes.js
@@ -21,7 +21,7 @@ module.exports = async (client, config) => {
             console.log(
               `\x1b[31m  〢`,
               `\x1b[33m ${moment(Date.now()).format("lll")}`,
-              `\x1b[34m${interaction.user.username} ANSWERED`,
+              `\x1b[34m ${interaction.user.username} ANSWERED`,
               `\x1b[35m Yes`,
             );
             const languges = new MessageActionRow().addComponents(
